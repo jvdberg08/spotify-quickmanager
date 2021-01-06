550f9028f778e73c5e6fb09b7bd6aa6013e1fd58
jvdberg08 <jvdberg080403@gmail.com>
2021-01-06 23:38:20 +0100

    Removed unnecessary dependency and updated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import VueCookies from 'vue-cookies'
 import BootstrapVue from 'bootstrap-vue';
 import axios from 'axios'
 
@@ -9,11 +8,8 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(VueCookies)
 Vue.use(BootstrapVue)
-
 Vue.prototype.$axios = axios
-
 Vue.config.productionTip = false
 
 new Vue({
